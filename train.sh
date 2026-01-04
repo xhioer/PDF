0120b351dc23a3173ee551692ef0f62ded673de5
chenhao2345 <67144451+chenhao2345@users.noreply.github.com>
2026-01-04 16:49:45 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=8
+export CUDA_VISIBLE_DEVICES=0
 # python -m torch.distributed.run --nproc_per_node=1 --master_port 12242 clip_finetune.py --dataset prcc --cfg configs/prcc.yaml #
 python -m torch.distributed.run --nproc_per_node=1 --master_port 12336 clip_finetune.py --dataset ltcc --cfg configs/ltcc.yaml #
 #python -m torch.distributed.run --nproc_per_node=1 --master_port 12346 clip_finetune.py --dataset vcclothes --cfg configs/vcclothes.yaml #
